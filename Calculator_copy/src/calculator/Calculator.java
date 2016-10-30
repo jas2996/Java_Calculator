3d622c23e84ad705a81e40db22b7134f758e72a9
jas2996 <JaskiranLamba@Jaskirans-MacBook-Pro.local>
2016-10-30 14:08:15 -0400

    added the files

---
+++ Calculator_copy/src/calculator/Calculator.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author JaskiranLamba
+ */
+public class Calculator {// a very simple calculator app
+  public Calculator(){
+    
+  }
+  public int add(int a, int b){
+    return a + b;     
+  }
+  public int subtract(int a, int b){
+    return a - b;
+  }
+  public int multiply(int a, int b){
+    return a * b;
+  }
+  public int divide(int a, int b){
+    if (b == 0){
+     System.out.println("Error! Dividing by zero is not allowed.");  
+      return 0;
+    } else {
+        return a/b;
+    }           
+  }
+  public int modulo(int a, int b){
+    if (b==0){
+      System.out.println("Error! Dividing by zero is not allowed.");
+      	return 0;
+    } else {
+      return a%b;
+    }
+  }
+  public static void main (String[] args){
+    Calculator myCalculator = new Calculator();
+    	System.out.println(myCalculator.add(5,7));
+    	System.out.println(myCalculator.subtract(45,11));
+  }
+}
